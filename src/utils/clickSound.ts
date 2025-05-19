b9981c080cb82fb636fd171a641cdd27a64a528c
dixitmalviya10 <dixitmalviya10@gmail.com>
2025-05-19 21:29:11 +0530

    fix: added pop sound

--- src/utils/clickSound.ts
+++ src/utils/clickSound.ts
@@ -1,7 +1,6 @@
 import popSound from "../assets/sounds/pop-sound.mp3";
 
 export function playClickSound() {
-  // const audio = new Audio(popSound);
-  // audio.play();
-  return;
+  const audio = new Audio(popSound);
+  audio.play();
 }
