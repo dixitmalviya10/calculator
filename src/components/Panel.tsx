49044651429401aaf0b9c4865807a0ee6d462a63
dixitmalviya10 <dixitmalviya10@gmail.com>
2025-05-19 13:43:46 +0530

    fix: fixed page breaking issue

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,7 +1,4 @@
 const Panel = () => {
-  const history = localStorage.getItem("history");
-  const historyData = JSON.parse(history || "").split("=") || [];
-
   return (
     <div className="font-semibold min-h-screen shadow-md fixed right-0 top-0 bottom-0 left-[80%] px-8 py-10 bg-slate-300 border-l-4 border-slate-400">
       <div className="flex justify-between items-center">
@@ -11,9 +8,7 @@ const Panel = () => {
         </button>
       </div>
       <ul className="mt-16 text-right text-lg tracking-wider">
-        {historyData.map((data: number, index: number) => (
-          <li key={index}>{data}</li>
-        ))}
+        <li>10000</li>
       </ul>
     </div>
   );
